f9f0fb212d3cd9c8fa43b54ffbf29a9c6ac58b96
jrccrj <75252027+CPT-Jack-A-Castle@users.noreply.github.com>
2023-03-01 23:37:54 +0000

    Update AssemblyInfo.cs

--- Clipper/Clipper/Properties/AssemblyInfo.cs
+++ Clipper/Clipper/Properties/AssemblyInfo.cs
@@ -17,9 +17,13 @@ using System.Runtime.InteropServices;
 // Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
 // для компонентов COM. Если необходимо обратиться к типу в этой сборке через
 // из модели COM задайте для атрибута ComVisible этого типа значение true.
+//Setting ComVisible to False makes the types in this assembly invisible
+// for COM components. If you need to refer to a type in this assembly via
+// from the COM model, set the ComVisible attribute of this type to true.
 [assembly: ComVisible(false)]
 
 // Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+//The following GUID represents the typelib identifier if this project is accessible from COM
 [assembly: Guid("9debd99e-2b66-47b6-a327-36c777e380ef")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
@@ -31,6 +35,16 @@ using System.Runtime.InteropServices;
 //
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+//Assembly version information consists of the following four values:
+//
+// Major version number
+// Minor version number
+// build number
+// revision number
+//
+// You can set all values or accept the default build and revision numbers
+// using "*" as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
